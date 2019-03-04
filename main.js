73248e90c9e26f031bab1910a4ef7d5264e05059
Mark McCullough <mark.mccullough@modmed.com>
2019-03-04 07:50:42 -1000

    css changes, cmd+quit support, update gitignore

--- main.js
+++ main.js
@@ -55,7 +55,8 @@ function createWindow () {
           { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
           { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
           { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" },
+          { role: 'quit' }
       ]}
   ];
 
